c830698f8e8ffb582351ce290bd2ffd351281795
jceyy <jc.ringenbach@gmail.com>
2025-04-22 12:15:53 +0200

    Changes to CMakeLists.txt to accomodate windows compilation

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <string>
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif
+
 #include "../include/PRL/Game.hpp"
 using namespace std;
 
 int main(int argc, char** argv) {
+    #if defined(_WIN32) || defined(_WIN64)
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+    #endif // Windows specific code to open a console window
+    
     std::cout << "Argument number : " << argc << std::endl;
     for (int i(0); i < argc; ++i) std::cout << argv[i] << std::endl;
     std::cout << std::endl;
